dc27418dd7e6de4696b60fb783677ae3ca5ce315
Etienne Carriere <etienne.carriere@linaro.org>
2018-04-17 09:50:21 +0200

    sks ta: fix handle database against spectre issue
    Caller handle value is processed to generate validation masks so that
    invalid handles are force to null value hence treated as invalid
    handle argument. The sequence for generating the validation mask
    does not rely on conditioned execution that could be bypassed through
    exploits on speculative execution.
    
    The sequence is inspired from the Arm64 array_index_mask_nospec macro
    recently merged into the Linux kernel.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- ta_services/secure_key_services/src/handle.c
+++ ta_services/secure_key_services/src/handle.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <assert.h>
 #include <stdlib.h>
 #include <tee_internal_api.h>
 #include <tee_internal_api_extensions.h>
@@ -49,6 +50,10 @@ uint32_t handle_get(struct handle_db *db, void *ptr)
 		new_max_ptrs = db->max_ptrs * 2;
 	else
 		new_max_ptrs = HANDLE_DB_INITIAL_MAX_PTRS;
+
+	if (new_max_ptrs > (UINT32_MAX / 2))
+		return 0;
+
 	p = TEE_Realloc(db->ptrs, new_max_ptrs * sizeof(void *));
 	if (!p)
 		return 0;
@@ -62,22 +67,43 @@ uint32_t handle_get(struct handle_db *db, void *ptr)
 	return n;
 }
 
+/*
+ * Protect from speculative access based on out bound handle values:
+ * Generate a validation mask from signed difference from max valid handle
+ * value to argument handle value:
+ * - out bound handle values generate a null mask.
+ * - handle values that can be seen as negative index generate a null mask.
+ * This scheme expects value 0 is an invalid handle value and should
+ * always return an invalid reference: here always a NULL reference.
+ */
+static size_t guard_handle(struct handle_db *db, uint32_t handle)
+{
+	uint32_t mask_neg = ((int32_t)handle) >> (sizeof(handle) * 8 - 1);
+	uint32_t udiff = db->max_ptrs - handle;
+	int32_t diff = ((int32_t)udiff) >> (sizeof(udiff) * 8 - 1);
+	uint32_t mask_ovf = diff;
+
+	return handle & ~mask_ovf & ~mask_neg;
+}
+
 void *handle_put(struct handle_db *db, uint32_t handle)
 {
+	uint32_t index;
 	void *p;
 
-	if (!db || !handle || handle >= db->max_ptrs)
+	if (!db)
 		return NULL;
 
-	p = db->ptrs[handle];
-	db->ptrs[handle] = NULL;
+	index = guard_handle(db, handle);
+	p = db->ptrs[index];
+	db->ptrs[index] = NULL;
 	return p;
 }
 
 void *handle_lookup(struct handle_db *db, uint32_t handle)
 {
-	if (!db || !handle || handle >= db->max_ptrs)
+	if (!db)
 		return NULL;
 
-	return db->ptrs[handle];
+	return db->ptrs[guard_handle(db, handle)];
 }
