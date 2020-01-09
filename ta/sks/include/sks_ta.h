f4aa73260356657508a38c88ce715f7da662e796
Markus S. Wamser <markus.wamser@mixed-mode.de>
2020-01-09 10:51:45 +0100

    [review] sks ta: typo fixes in comments
    Correct some minor typos in TA header file.
    
    Signed-off-by: Markus S. Wamser <markus.wamser@mixed-mode.de>

--- ta/sks/include/sks_ta.h
+++ ta/sks/include/sks_ta.h
@@ -24,27 +24,27 @@
 #define SKS_TRUE				1
 
 /*
- * Note on SKS TA commands ABI
+ * Note on SKS TA commands API
  *
  * For evolution of the TA API and to not mess with the GPD TEE 4 parameters
- * constraint, all the SKS TA invocation commands use a subset of available the
+ * constraint, all the SKS TA invocation commands use a subset of the available
  * GPD TEE invocation parameter types.
  *
  * Param#0 is used for the so-called control arguments of the invoked command
  * and for providing a PKCS#11 compliant status code for the request command.
  * Param#0 is an in/out memory reference (aka memref[0]). The input buffer
  * stores the command arguments serialized inside. The output buffer will
- * store the 32bit SKS retrun code for the command. Client shall get this
+ * store the 32bit SKS return code for the command. Client shall get this
  * return code and override the GPD TEE Client API legacy TEE_Result value.
  *
  * Param#1 is used for input data arguments of the invoked command.
- * It is unused or is a input memory reference, aka memref[1].
+ * It is unused or is an input memory reference, aka memref[1].
  * Evolution of the API may use memref[1] for output data as well.
  *
  * Param#2 is mostly used for output data arguments of the invoked command
  * and for output handles generated from invoked commands.
  * Few commands uses it for a secondary input data buffer argument.
- * It is unused or is a input/output/in-out memory reference, aka memref[2].
+ * It is unused or is an input/output/in-out memory reference, aka memref[2].
  *
  * Param#3 is currently unused and reserved for evolution of the API.
  */
@@ -52,7 +52,7 @@
 /*
  * SKS_CMD_PING		Acknowledge TA presence and return TA version info
  *
- * Optinal invocation parameter (if none, command simply returns with success)
+ * Optional invocation parameter (if none, command simply returns with success)
  * [out]        memref[2] = [
  *                      32bit version major value,
  *                      32bit version minor value
