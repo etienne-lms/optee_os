3775a8279a044459e0c92bd9d1cba233342fe847
Etienne Carriere <etienne.carriere@linaro.org>
2021-09-21 09:36:21 +0200

    core: release driver probing resource after initialization
    Use release_init_resource initcall level to release DT phandle provider
    list, probe list and ready list.
    
    Change-Id: Ie6f3c1ab5cfbf8ef98d8a6418bfa079105b4b5ec
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/kernel/dt_driver.c
+++ core/kernel/dt_driver.c
@@ -94,7 +94,8 @@ static TEE_Result dt_driver_release_provider(void)
 
 	return TEE_SUCCESS;
 }
-driver_init_late(dt_driver_release_provider);
+
+release_init_resource(dt_driver_release_provider);
 
 /*
  * Helper functions for dt_drivers quering driver provider information
@@ -652,6 +653,25 @@ static TEE_Result probe_dt_drivers(void)
 }
 
 driver_init(probe_dt_drivers);
+
+static TEE_Result release_probe_lists(void)
+{
+	struct dt_driver_probe *elt = NULL;
+	struct dt_driver_probe *next = NULL;
+	const void * __maybe_unused fdt = get_embedded_dt();
+
+	assert(fdt && TAILQ_EMPTY(&dt_driver_probe_list));
+
+	TAILQ_FOREACH_SAFE(elt, &dt_driver_ready_list, link, next) {
+		DMSG("element: %s on node %s", elt->dt_drv->name,
+		     fdt_get_name(fdt, elt->nodeoffset, NULL));
+		free(elt);
+	}
+
+	return TEE_SUCCESS;
+}
+
+release_init_resource(release_probe_lists);
 
 /*
  * Simple bus support: handy to parse subnodes
