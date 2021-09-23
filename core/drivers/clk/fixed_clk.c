0ecb797f5040667e4a4609864871e2d96f9c69d4
Etienne Carriere <etienne.carriere@linaro.org>
2021-09-23 12:11:06 +0200

    drivers: clk: move fixed clock to orphan clocks
    Allocate fixed clocks as orphan clocks since it is what they are.
    
    Add pager constraint for fixed clock to be available during low
    power transitions while pager does not execute.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/clk/fixed_clk.c
+++ core/drivers/clk/fixed_clk.c
@@ -7,10 +7,12 @@
 #include <drivers/clk_dt.h>
 #include <libfdt.h>
 #include <malloc.h>
+#include <mm/core_memprot.h>
 #include <stdint.h>
 
 struct fixed_clock_data {
 	unsigned long rate;
+	const char *name;
 };
 
 static unsigned long fixed_clk_get_rate(struct clk *clk,
@@ -21,10 +23,19 @@ static unsigned long fixed_clk_get_rate(struct clk *clk,
 	return d->rate;
 }
 
+static const char *fixed_clk_get_name(struct clk *clk)
+{
+	struct fixed_clock_data *d = clk->priv;
+
+	return d->name;
+}
+
 static const struct clk_ops fixed_clk_clk_ops = {
-	.id = CLK_OPS_ELT,
+	.id = CLK_OPS_ORPHAN,
 	.get_rate = fixed_clk_get_rate,
+	.get_name = fixed_clk_get_name,
 };
+DECLARE_KEEP_PAGER(fixed_clk_ops);
 
 static TEE_Result fixed_clock_setup(const void *fdt, int offs)
 {
@@ -33,42 +44,56 @@ static TEE_Result fixed_clock_setup(const void *fdt, int offs)
 	struct clk *clk = NULL;
 	TEE_Result res = TEE_ERROR_GENERIC;
 	struct fixed_clock_data *fcd = NULL;
+	char *dup_name = NULL;
 
 	name = fdt_get_name(fdt, offs, NULL);
 	if (!name)
 		name = "fixed-clock";
 
-	clk = clk_alloc(name, &fixed_clk_clk_ops, NULL, 0);
-	if (!clk)
-		return TEE_ERROR_OUT_OF_MEMORY;
+	if (!is_unpaged((void *)name)) {
+		dup_name = strdup(name);
+		if (!dup_name) {
+			res = TEE_ERROR_OUT_OF_MEMORY;
+			goto err;
+		}
+		name = dup_name;
+	}
+
+	clk = clk_alloc_orphans(&fixed_clk_clk_ops, 1);
+	if (!clk) {
+		res = TEE_ERROR_OUT_OF_MEMORY;
+		goto err;
+	}
 
 	fcd = calloc(1, sizeof(struct fixed_clock_data));
 	if (!fcd) {
 		res = TEE_ERROR_OUT_OF_MEMORY;
-		goto free_clk;
+		goto err;
 	}
 
 	freq = fdt_getprop(fdt, offs, "clock-frequency", NULL);
 	if (!freq) {
 		res = TEE_ERROR_BAD_FORMAT;
-		goto free_fcd;
+		goto err;
 	}
 
 	fcd->rate = fdt32_to_cpu(*freq);
+	fcd->name = name;
+
 	clk->priv = fcd;
 
 	res = clk_register(clk);
 	if (res)
-		goto free_fcd;
+		goto err;
 
 	res = clk_dt_register_clk_provider(fdt, offs, clk_dt_get_simple_clk,
 					   clk);
 	if (!res)
 		return TEE_SUCCESS;
 
-free_fcd:
+err:
+	free(dup_name);
 	free(fcd);
-free_clk:
 	clk_free(clk);
 
 	return res;
