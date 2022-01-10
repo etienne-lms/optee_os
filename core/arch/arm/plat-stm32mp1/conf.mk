c878faf8c421ea57df672fde3c2784fc6823945d
Etienne Carriere <etienne.carriere@st.com>
2022-01-10 12:14:11 +0100

    plat-stm32mp1: conf: enable watchdog support
    Add the watchdog enable by default on STM32MP1 platform.
    
    Signed-off-by: Lionel Debieve <lionel.debieve@foss.st.com>
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- core/arch/arm/plat-stm32mp1/conf.mk
+++ core/arch/arm/plat-stm32mp1/conf.mk
@@ -70,6 +70,7 @@ $(call force,CFG_DRIVERS_CLK_DT,n)
 $(call force,CFG_STM32_CRYP,n)
 $(call force,CFG_STM32_GPIO,n)
 $(call force,CFG_STM32_I2C,n)
+$(call force,CFG_STM32_IWDG,n)
 $(call force,CFG_STPMIC1,n)
 $(call force,CFG_STM32MP1_SCMI_SIP,n)
 $(call force,CFG_SCMI_PTA,n)
@@ -82,6 +83,7 @@ CFG_STM32_CRYP ?= y
 CFG_STM32_ETZPC ?= y
 CFG_STM32_GPIO ?= y
 CFG_STM32_I2C ?= y
+CFG_STM32_IWDG ?= y
 CFG_STM32_RNG ?= y
 CFG_STM32_RSTCTRL ?= y
 CFG_STM32_UART ?= y
@@ -101,6 +103,8 @@ endif
 
 CFG_DRIVERS_RSTCTRL ?= $(CFG_STM32_RSTCTRL)
 $(eval $(call cfg-depends-all,CFG_STM32_RSTCTRL,CFG_DRIVERS_RSTCTRL))
+
+CFG_WDT ?= $(CFG_STM32_IWDG)
 
 # Platform specific configuration
 CFG_STM32MP_PANIC_ON_TZC_PERM_VIOLATION ?= y
