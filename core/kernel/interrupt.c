ae9eac2942b83ceb9228d8297aa4e2f24c9b9491
Etienne Carriere <etienne.carriere@linaro.org>
2023-05-31 10:42:14 +0200

    core: interrupt: interrupt_get_main_chip() returns main controller
    Adds helper function interrupt_get_main_chip() to get the struct itr_chip
    reference of the CPU main interrupt controller (e.g. the GIC). This
    function helps adapting a generic interrupt controller framework to
    consider CPU main interrupt controller specific reference.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/kernel/interrupt.c
+++ core/kernel/interrupt.c
@@ -28,6 +28,11 @@ void interrupt_main_init(struct itr_chip *chip)
 	itr_chip = chip;
 }
 
+struct itr_chip *interrupt_get_main_chip(void)
+{
+	return itr_chip;
+}
+
 #ifdef CFG_DT
 int dt_get_irq_type_prio(const void *fdt, int node, uint32_t *type,
 			 uint32_t *prio)
